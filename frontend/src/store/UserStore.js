2ea3bcfaa57917bd4ec86ed12dd4490452472cc3
Andrei Siarheyenka <84916548+AndrewSRGN@users.noreply.github.com>
2024-05-31 10:04:28 -0700

    Create User Store

---
+++ frontend/src/store/UserStore.js
@@ -0,0 +1,25 @@
+import {makeAutoObservable} from "mobx";
+
+export default class UserStore {
+    constructor() {
+        this._isAuth = false;
+        this._user = {};
+        makeAutoObservable(this);
+    }
+
+    set isAuth(boolean) {
+        this._isAuth = boolean;
+    }
+
+    set user(user) {
+        this._user = user;
+    }
+
+    get isAuth() {
+        return this._isAuth;
+    }
+
+    get user() {
+        return this._user;
+    }
+}
